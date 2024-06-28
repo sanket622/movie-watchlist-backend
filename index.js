a03d88f943b5f35f7eddc42705e5c5ee2d190d74
sanket622 <sanketkumar0068@gmail.com>
2024-06-28 09:38:24 +0530

    update

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use('/api/movies', moviesRouter);
