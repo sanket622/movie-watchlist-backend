bbd7a63a26238153f8b9f8fc62dc4061a230a813
sanket622 <sanketkumar0068@gmail.com>
2024-07-01 20:31:27 +0530

    update

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Movie Watchlist API');
+});
+
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
