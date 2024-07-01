67552caf6759913a0875c85a04e266830bd17d39
sanket622 <sanketkumar0068@gmail.com>
2024-07-01 16:52:24 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
-
-module.exports = {connection}
 
 app.use('/api/movies', moviesRouter);
 
