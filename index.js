d4d69b7114fbaf0519309d0cd8f41ed1da31f7a7
sanket622 <sanketkumar0068@gmail.com>
2024-10-09 08:55:34 +0530

    Update

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: 'https://movie-watchlist-frontend.vercel.app/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
